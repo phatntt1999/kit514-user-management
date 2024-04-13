45258e6e84c2dbaa85191dd29b3e52c65f6357c0
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2024-04-14 09:06:31 +1000

    Added a new view for part 3

---
+++ view/ViewBackToHome.php
@@ -0,0 +1,14 @@
+<?php
+//not used until Part 3
+class ViewBackToHome
+{
+    public function output()
+    {
+        ?>
+        <div>
+            <a href="index.php">Back to Home</a>
+        </div>
+        <?php
+    }
+}
+?>
